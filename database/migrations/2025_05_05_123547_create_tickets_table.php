34e23133201f296023525f5f3b0b49004bcce1a5
maksudbhuiyan595 <mdmaksudbhuiyan595@gmail.com>
2025-05-05 19:23:02 +0600

    database create

---
+++ database/migrations/2025_05_05_123547_create_tickets_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject',255);
+            $table->text('description')->nullable();
+            $table->enum('category',['Technical','Billing','General'])->default('General');
+            $table->enum('priority',['Low','Medium','High'])->default('High');
+            $table->string('attachment')->nullable();
+            $table->enum('status',['Open','In-progress','Resolved','Closed'])->default('Open');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
